7a5346b0590712781ae5fc76d98e47da4b73a334
Jeremy Emata <jeremy.emata@gmail.com>
2018-11-06 03:35:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
   
   def self.create_table
         sql =  <<-SQL 
